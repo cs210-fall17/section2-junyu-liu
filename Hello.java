384cb26f1dba030a819bc25bcd126387882b77ba
junyuliu1994 <junyuliu@email.arizona.edu>
2017-12-10 15:17:42 -0700

    red

--- Hello.java
+++ Hello.java
@@ -25,6 +25,7 @@ public class Hello {
 
     public static void redLeader() {
         System.out.println("I copy, gold leader!");
+	System.out.println("red change");
         // Red Leader should add a print statement that says "I copy, gold leader!"
 
 
